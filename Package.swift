6b3f73657db4bb5583f3f126ddfb2f40be336dc1
Tony <duanhai@outlook.com>
2018-10-19 11:14:14 +0800

    add cer & remote fluent-sqlite

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+//        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0")
 
 
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor","MongoKitten"]),
+        .target(name: "App", dependencies: ["Vapor","MongoKitten"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
